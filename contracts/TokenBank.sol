b7346693846f9ab5aacf79d9d5ebd9e5598ed23d
umiumiu25 <20010129kk@gmail.com>
2023-10-28 23:24:13 +0900

    TokenBank:イベント定義

--- contracts/TokenBank.sol
+++ contracts/TokenBank.sol
@@ -3,24 +3,37 @@
 pragma solidity ^0.8.9;
 
 contract TokenBank {
-  // Tokenの名前
-  string private _name;
+    /// @dev Tokenの名前
+    string private _name;
 
-  // Tokenのシンボル
-  string private _symbol;
+    /// @dev Tokenのシンボル
+    string private _symbol;
 
-  // Tokenの総供給数
-  uint256 constant _totalSupply = 1000;
+    /// @dev Tokenの総供給数
+    uint256 constant _totalSupply = 1000;
 
-  // TokenBankが預かっているTokenの総額
-  uint256 private _bankTotalDeposite;
+    /// @dev TokenBankが預かっているTokenの総額
+    uint256 private _bankTotalDeposit;
 
-  // TokenBankのオーナー
-  address public owner;
+    /// @dev TokenBankのオーナー
+    address public owner;
 
-  // アカウントアドレス毎のTokenBankが預けたToken数
-  mapping(address => uint256) private _balances;
+    /// @dev アカウントアドレス毎のTokenBankが付与したToken数
+    mapping(address => uint256) private _balances;
 
-  // アカウントアドレス毎のTokenBankが預かっているToken数
-  mapping(address => uint256) private _tokenBankBalanaces;
+    /// @dev アカウントアドレス毎のTokenBankが預かっているToken数
+    mapping(address => uint256) private _tokenBankBalanaces;
+
+    /// @dev Token移転時のイベント
+    event TokenTransfar(
+        address indexed from,
+        address indexed to,
+        uint256 amount
+    );
+
+    /// @dev Token預入時のイベント
+    event TokenDeposit(address indexed from, uint256 amount);
+
+    /// @dev Token引出時のイベント
+    event TokenWithdraw(address indexed from, uint256 amount);
 }
