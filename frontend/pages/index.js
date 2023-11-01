a42abdd9dfae830ff87fc527f68dd6c5bced21e2
umiumiu25 <20010129kk@gmail.com>
2023-11-01 17:29:27 +0900

    frontend:トークン預入機能実装

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -119,6 +119,32 @@ export default function Home() {
     }
   };
 
+  const tokenDeposit = async (e) => {
+    e.preventDefault();
+    if (tokenBalance >= inputData.depositAmount) {
+      try {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const tokenBankContract = new ethers.Contract(tokenBankAddress, TokenBank.abi, signer);
+        const tx = await tokenBankContract.deposit(inputData.depositAmount);
+        await tx.wait();
+
+        const tBalance = await tokenBankContract.balanceOf(account);
+        setTokenBalance(tBalance.toNumber());
+        const bBalance = await tokenBankContract.bankBalanceOf(account);
+        setBankBalance(bBalance.toNumber());
+        setInputData(prevData => ({
+          ...prevData,
+          depositAmount: '',
+        }));
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      alert("所持残高を超えるトークンは預入できません");
+    }
+  };
+
   const handler = (e) => {
     setInputData(prevData => ({
       ...prevData,
@@ -202,6 +228,20 @@ export default function Home() {
                           className="w-2/12 mx-2 bg-white border-blue-500 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border hover:border-transparent rounded"
                           onClick={tokenTransfer}
                         >移転</button>
+                      </form>
+                      <form className="flex pl-1 py-1 mb-1 bg-white border border-gray-400">
+                        <input
+                          type="text"
+                          className="w-10/12 ml-2 text-right border border-gray-400"
+                          name="depositAmount"
+                          placeholder={`100`}
+                          onChange={handler}
+                          value={inputData.depositAmount}
+                        />
+                        <button
+                          className="w-2/12 mx-2 bg-white hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded"
+                          onClick={tokenDeposit}
+                        >預入</button>
                       </form>
                     </>) : (<></>)}
                 </div>
