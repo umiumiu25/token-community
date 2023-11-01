80878e0ddb7f55b372e90374f8fe7256cdcda8b8
umiumiu25 <20010129kk@gmail.com>
2023-11-01 18:21:16 +0900

    frontend:所有NFT情報取得と表示

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -83,12 +83,32 @@ export default function Home() {
     const provider = new ethers.providers.Web3Provider(ethereum);
     const signer = provider.getSigner();
 
-    const nftContract = new ethers.Contract(memberNFTAddress, MemberNFT.abi, signer);
+    const memberNFTContract = new ethers.Contract(memberNFTAddress, MemberNFT.abi, signer);
 
-    const balance = await nftContract.balanceOf(addr);
+    const balance = await memberNFTContract.balanceOf(addr);
     console.log(`nftBalances: ${balance.toNumber()}`);
     if (balance.toNumber() > 0) {
       setNftOwner(true);
+      setItems([]);
+      for (let i = 0; i < balance.toNumber(); i++) {
+        const tokenId = (await memberNFTContract.tokenOfOwnerByIndex(addr, i)).toNumber();
+        let tokenURI = await memberNFTContract.tokenURI(tokenId);
+        tokenURI = tokenURI.replace('ipfs://', 'https://ipfs.io/ipfs/');
+        const meta = await axios.get(tokenURI);
+
+        const name = meta.data.name;
+        const description = meta.data.description;
+        const imageURI = meta.data.image.replace('ipfs://', 'https://ipfs.io/ipfs/');
+
+        const item = {
+          tokenId,
+          name,
+          description,
+          tokenURI,
+          imageURI,
+        };
+        setItems(items => [...items, item]);
+      }
     } else {
       setNftOwner(false);
     }
@@ -290,6 +310,22 @@ export default function Home() {
                           onClick={tokenWithdraw}
                         >引出</button>
                       </form>
+                      {
+                        items.map((item, i) => (
+                          <div key={i} className="flex justify-center pl-1 py-2 mb-1">
+                            <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+                              <img className=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src={item.imageURI} alt="" />
+                              <div className="p-6 flex flex-col justify-start">
+                                <h5 className="text-gray-900 text-xl font-medium mb-2">{item.name}</h5>
+                                <p className="text-gray-700 text-base mb-4">
+                                  {item.description}
+                                </p>
+                                <p className="text-gray-600 text-xs">所有NFT# {item.tokenId}</p>
+                              </div>
+                            </div>
+                          </div>
+                        ))
+                      }
                     </>) : (<></>)}
                 </div>
                 :
